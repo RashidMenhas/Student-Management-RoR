88fd33b7217cb05eeaad49f35fde4cb657d61047
Rashid Menhas <rashidmenhas10@gmail.com>
2023-03-15 15:21:25 +0500

    create action for student app and render the form and show the notice .

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,50 @@
+class StudentsController < ApplicationController
+
+    before_action :set_student, only: %i[show edit update destory]
+
+    def index
+        @students = Student.all
+    end
+
+    def new
+        @student = Student.new
+    end
+
+    def create
+        @student = Student.new(student_params)
+        if @student.save
+            redirect_to students_path, notice:'Student has been created successfully.'
+        else
+            render new
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @student.update(student_params)
+            redirect_to student_path, notice:'Student has been updated successfully.'
+        else
+            render edit
+        end
+    end
+
+    def destory
+        @student.destory
+        redirect_to students_path, notice:'Student has been deleted successfully.'
+    end
+
+    private
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name, :email)
+    end
+
+    def set_student
+        @student = Student.find(params[:id])
+    end
+end
