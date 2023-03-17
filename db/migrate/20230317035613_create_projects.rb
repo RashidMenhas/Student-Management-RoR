8dcfc4c05cd45ed345b10fa32a872cee090ca101
Rashid Menhas <rashidmenhas10@gmail.com>
2023-03-17 09:02:26 +0500

    Create has_many_through association and create foreign key

---
+++ db/migrate/20230317035613_create_projects.rb
@@ -0,0 +1,10 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
