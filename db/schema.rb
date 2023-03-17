4bddff2e281f8922cabb25769deda67e28f93492
Rashid Menhas <rashidmenhas10@gmail.com>
2023-03-17 19:09:09 +0500

    Create validation and association for the project.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_17_035936) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_17_041425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_17_035936) do
     t.bigint "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "submission_data"
     t.index ["project_id"], name: "index_student_projects_on_project_id"
     t.index ["student_id"], name: "index_student_projects_on_student_id"
   end
