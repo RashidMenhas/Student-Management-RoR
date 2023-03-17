a8124aac238a0de86c92bf242591cd0fd2763ce3
Rashid Menhas <rashidmenhas10@gmail.com>
2023-03-17 19:07:51 +0500

    Create HAS_MANY ,BELONGS_TO, and HAS_MANY_THROUGH association.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,7 @@
 class Student < ApplicationRecord
     has_many :blogs
     has_and_belongs_to_many :courses
+    has_many :student_projects
+    has_many :projects, through: :student_projects  
     validates :first_name, :last_name, :email, presence: true
 end
