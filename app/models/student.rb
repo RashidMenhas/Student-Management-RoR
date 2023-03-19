ac1dfc1e4ec19ae576bc8016b40b49d900e79dca
Rashid Menhas <rashidmenhas10@gmail.com>
2023-03-19 22:00:31 +0500

    update project.

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,8 @@ class Student < ApplicationRecord
     has_many :student_projects
     has_many :projects, through: :student_projects  
     validates :first_name, :last_name, :email, presence: true
+    validates :email, uniqueness: true
+    validates :first_name, :last_name, length: {minimum: 5, maximum: 10}
+    validates :first_name, :last_name, formate: { with: }
+    
 end
