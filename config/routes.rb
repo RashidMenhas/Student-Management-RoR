da8a5941ae899fc24fc497b153c2e4e6d8cc744b
Rashid Menhas <rashidmenhas10@gmail.com>
2023-03-26 19:59:51 +0500

    Update routes file and made a migration and update seed file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
+  root 'students#index'
+
   resources :projects
   resources :courses
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'students#index'
-  # resources :students
+  resources :students
 
   # namespace :admin do
   #   resources :students
   # end
 
-  scope module: :admin do
-     resources :students 
-  end
+  # scope module: :admin do
+  #    resources :students 
+  # end
 end
